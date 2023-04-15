0a75342184bf94769def765c60262324d7c5008c
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-15 19:49:18 +0200

    settings changed in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module start {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
 
-    opens graphics to javafx.fxml;
-    exports graphics;
+    opens graphics.start to javafx.fxml;
+    exports graphics.start;
+    opens graphics.controllers to javafx.fxml;
+    exports graphics.controllers;
 }
