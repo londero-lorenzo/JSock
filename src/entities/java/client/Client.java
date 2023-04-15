9bf687f4ac6cb2e2fa24e96cd730d9aa65e85d0c
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-15 23:46:37 +0200

    added parameters, and pre-connection function

--- src/entities/java/client/Client.java
+++ src/entities/java/client/Client.java
@@ -1,4 +1,27 @@
 package client;
 
+import structures.Address;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Client {
+    private Address serverAddress;
+
+    private socket.Socket socket;
+
+
+    public void connectTo(Address serverAddress)
+    {
+        this.serverAddress = serverAddress;
+        this.connect();
+    }
+
+    private void connect()
+    {
+        this.socket = new socket.Socket(this.serverAddress);
+    }
 }
