1b36cbb36005e53356f554ae3619b5abb273b093
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-15 23:45:37 +0200

    added parameters, constructor and getters

--- src/entities/java/structures/Address.java
+++ src/entities/java/structures/Address.java
@@ -1,4 +1,25 @@
 package structures;
 
 public class Address {
+
+    private String ipv4;
+
+    private int port;
+
+    private Boolean secure = false;
+
+    public Address(String ipv4, int port)
+    {
+        this.ipv4 = ipv4;
+        this.port = port;
+    }
+
+
+    public String getIpv4() {
+        return ipv4;
+    }
+
+    public int getPort() {
+        return port;
+    }
 }
