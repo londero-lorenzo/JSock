511c44680dc34093507cb2b9fdcced3aa3c513d7
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-19 00:08:03 +0200

    changed reference to parent class, added send method

--- src/entities/java/socket/InputChannel.java
+++ src/entities/java/socket/InputChannel.java
@@ -1,5 +1,6 @@
 package socket;
 
+import client.ClientSocket;
 import structures.Message;
 
 import java.io.IOException;
@@ -9,18 +10,24 @@ public class InputChannel {
 
     private OutputStream socketOutputStream;
 
+    private ClientSocket socket;
 
-    public InputChannel(java.net.Socket socket)
-    {
+    public InputChannel(ClientSocket socket) {
+        this.socket = socket;
         try {
-            this.socketOutputStream = socket.getOutputStream();
+            this.socketOutputStream = this.socket.getSocketObject().getOutputStream();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void send(Message message)
-    {
-
+    public void send(Message message) {
+        try {
+            System.out.println(this.socket.getMessageSettings().getHeader());
+            this.socketOutputStream.write(this.socket.getMessageSettings().getHeader());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
