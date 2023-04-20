c1d39ef8d0a180fb204faadfaf8cfbfef794b23b
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-20 18:04:28 +0200

    created a class to storage all message types

---
+++ src/entities/java/structures/MessageTypes.java
@@ -0,0 +1,11 @@
+package structures;
+
+public class MessageTypes {
+
+    public static String SET_HEADER_LENGTH = "!SET_HEADER_LENGTH";
+    public static String TX_MESSAGE = "!MESSAGE";
+    public static String RX_NORMAL_MESSAGE = "!N_MSG";
+    public static String RX_LONG_MESSAGE = "!L_MSG";
+    public static String RX_END_LONG_MESSAGE = "!F_L_MSG";
+    public static String RECV_MSG = "!RECIVED";
+}
