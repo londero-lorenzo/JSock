c3d3ac999c02ad8ae1a90b037b621efb7d0f8ffb
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-20 17:38:57 +0200

    added message to send

--- src/main/java/commandLine/ClientStart.java
+++ src/main/java/commandLine/ClientStart.java
@@ -1,19 +1,20 @@
 package commandLine;
 
 import client.Client;
-import server.MultiConnectionServer;
 import structures.Address;
+import structures.Message;
+import structures.MessageTypes;
 import structures.MessageSettings;
 
 public class ClientStart {
 
-    public static void main(String[] args)
-    {
-        MessageSettings messageSettings = new MessageSettings(3, 2);
+    public static void main(String[] args) {
+        MessageSettings messageSettings = new MessageSettings(3);
         Client client = new Client(messageSettings);
         Address serverAddress = new Address("192.168.178.134", 30080);
         client.connectTo(serverAddress);
-        client.send();
+        Message message = new Message("Hello World", MessageTypes.TX_MESSAGE);
+        client.send(message);
     }
 
 }
