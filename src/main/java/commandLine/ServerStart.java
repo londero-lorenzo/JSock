53e45948a70cb57010ba1f6d97662ac0e8f8a0a2
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-20 18:08:43 +0200

    start server using Address object

--- src/main/java/commandLine/ServerStart.java
+++ src/main/java/commandLine/ServerStart.java
@@ -1,14 +1,16 @@
 package commandLine;
 
 import server.MultiConnectionServer;
+import structures.Address;
 import structures.MessageSettings;
 
 public class ServerStart {
 
     public static void main(String[] args)
     {
-        MessageSettings messageSettings = new MessageSettings(3, 2);
-        MultiConnectionServer multiConnectionServer = new MultiConnectionServer(messageSettings, 30080);
+        MessageSettings messageSettings = new MessageSettings(3);
+        Address serverAddress = new Address("192.168.178.134", 30080);
+        MultiConnectionServer multiConnectionServer = new MultiConnectionServer(serverAddress, messageSettings);
         multiConnectionServer.accept();
     }
 
