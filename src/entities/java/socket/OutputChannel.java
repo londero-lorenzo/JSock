5f8fe9499ca3165885396508237afbf7cd580058
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-20 18:07:32 +0200

    changed input to output stream, added method that allows to send messages

--- src/entities/java/socket/OutputChannel.java
+++ src/entities/java/socket/OutputChannel.java
@@ -1,33 +1,36 @@
 package socket;
 
 import client.ClientSocket;
+import structures.Message;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
 
 public class OutputChannel {
 
-    private InputStream socketInputStream;
+    private OutputStream socketOutputStream;
 
     private ClientSocket socket;
-
 
     public OutputChannel(ClientSocket socket) {
         this.socket = socket;
         try {
-            this.socketInputStream = this.socket.getSocketObject().getInputStream();
+            this.socketOutputStream = this.socket.getSocketObject().getOutputStream();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void read() {
-        int data;
+    public void send(Message message) {
         try {
-            data = this.socketInputStream.read();
+            System.out.println("Header length: " + this.socket.getMessageSettings().getHeaderSize());
+            System.out.println("Byte message: " + Arrays.toString(message.toString().getBytes()));
+            this.socketOutputStream.write(message.getType().getBytes());
+            this.socketOutputStream.write(message.getBytes());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(data);
     }
+
 }
