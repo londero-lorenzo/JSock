6274c0405ae7d40a5778b73b92e1f696f25293de
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-20 17:46:48 +0200

    removed 'maxHeaderLength' parameter, changed header creation

--- src/entities/java/structures/MessageSettings.java
+++ src/entities/java/structures/MessageSettings.java
@@ -5,29 +5,25 @@ import exceptions.MessageHeaderLengthException;
 public class MessageSettings {
 
 
-    private final int maxHeaderLength;
-
     private final int header;
 
-    public MessageSettings(int header, int maxHeaderLength) {
-        this.header = header;
-        this.maxHeaderLength = maxHeaderLength;
-        this.checkHeaderLength();
+    public MessageSettings(int headerByte) {
+        this.header = headerByte;
     }
 
 
-    private void checkHeaderLength() {
-        if (this.maxHeaderLength < Integer.toString(this.header).length())
-            throw new MessageHeaderLengthException(this);
+    // TODO: controlli vari
+    //private void checkHeaderLength() {
+    //    if (this.maxHeaderLength < Integer.toString(this.header).length())
+    //        throw new MessageHeaderLengthException(this);
+    //}
+
+
+    public int getHeaderSize() {
+
+        String headerString = "";
+        for (int i = 0; i < this.header; i++)
+            headerString += "9";
+        return Integer.parseInt(headerString);
     }
-
-
-    public int getHeader() {
-        return this.header;
-    }
-
-    public int getMaxHeaderLength() {
-        return this.maxHeaderLength;
-    }
-
 }
