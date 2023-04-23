dc36222ba6e2a97b3609ae577306f1e02418a656
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-24 00:08:39 +0200

    added methods to read partially or totally incoming bytes

--- src/entities/java/socket/DataInputReader.java
+++ src/entities/java/socket/DataInputReader.java
@@ -1,6 +1,10 @@
 package socket;
 
+import exceptions.MessageHeaderLengthException;
+import structures.MessageTypes;
+
 import java.io.IOException;
+import java.util.Arrays;
 
 public class DataInputReader {
 
@@ -13,19 +17,64 @@ public class DataInputReader {
         this.byteList = new ByteList();
     }
 
-    public ByteList read() {
-
-        for (int i = 0; i < this.inputChannel.getClientSocket().getMessageSettings().getHeaderSize(); i++) {
+    private void readAll(int times) {
+        boolean endFound = false;
+        for (int i = 0; i < times; i++) {
             try {
-                this.byteList.add((byte) this.inputChannel.getSocketInputStream().read());
+                byte c = (byte) this.inputChannel.getSocketInputStream().read();
+                if (Arrays.equals(new byte[]{this.byteList.getLastByte(), c}, MessageTypes.LINE_SEPARATOR.getBytes())) {
+                    this.byteList.removeLastByte();
+                    endFound = true;
+                    break;
+                }
+                this.byteList.add(c);
+
             } catch (IOException e) {
-                if (e instanceof java.net.SocketException && this.byteList.getLength() < this.inputChannel.getClientSocket().getMessageSettings().getHeaderSize())
-                    return this.byteList;
-                else
+                if (!(e instanceof java.net.SocketException && this.byteList.getLength() <= times))
                     throw new RuntimeException(e);
             }
         }
+        if (!endFound)
+            throw new MessageHeaderLengthException(this.inputChannel.getClientSocket().getMessageSettings());
+    }
+
+
+    /**
+     * Questo metodo leggerà ogni singolo byte in ricezione dal server a cui è connesso.
+     * <p>
+     * La lista di caratteri ricevuti conterranno tutte le informazioni per stabilire una connessione
+     *
+     * @return
+     */
+    public ByteList readAll() {
+        this.clear();
+        this.readAll(this.inputChannel.getClientSocket().getMessageSettings().getHeaderSize());
         return this.byteList;
 
+    }
+
+    private void read(int times) {
+        for (int i = 0; i < times; i++) {
+            try {
+                byte c = (byte) this.inputChannel.getSocketInputStream().read();
+                this.byteList.add(c);
+            } catch (IOException e) {
+                if (!(e instanceof java.net.SocketException && this.byteList.getLength() <= times))
+                    throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    public ByteList read() {
+        this.clear();
+        this.read(this.inputChannel.getClientSocket().getMessageSettings().getHeaderLength());
+        int messageLength = this.byteList.getIntFromByteList();
+        this.read(messageLength);
+        return this.byteList;
+    }
+
+    private void clear() {
+        this.byteList.clear();
     }
 }
