8880927719abe0f54ae02f4157d202f91b9803ee
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-04-24 00:32:27 +0200

    added header setting

--- src/main/java/commandLine/ServerStart.java
+++ src/main/java/commandLine/ServerStart.java
@@ -3,12 +3,13 @@ package commandLine;
 import server.MultiConnectionServer;
 import structures.Address;
 import structures.MessageSettings;
+import structures.Setting;
 
 public class ServerStart {
 
-    public static void main(String[] args)
-    {
-        MessageSettings messageSettings = new MessageSettings(3);
+    public static void main(String[] args) {
+        Setting<Integer> headerSetting = new Setting<>(MessageSettings.HEADER_SETTING, 3);
+        MessageSettings messageSettings = new MessageSettings(headerSetting);
         Address serverAddress = new Address("192.168.178.134", 30080);
         MultiConnectionServer multiConnectionServer = new MultiConnectionServer(serverAddress, messageSettings);
         multiConnectionServer.accept();
