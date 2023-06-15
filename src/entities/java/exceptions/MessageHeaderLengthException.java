7f3b774cf506590cb28aedeab664876a7b189a09
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-06-15 23:12:16 +0200

    formatted output warning

--- src/entities/java/exceptions/MessageHeaderLengthException.java
+++ src/entities/java/exceptions/MessageHeaderLengthException.java
@@ -4,6 +4,6 @@ import structures.MessageSettings;
 
 public class MessageHeaderLengthException extends RuntimeException {
     public MessageHeaderLengthException(MessageSettings messageSettings) {
-        super("There are not enough header byte to receive the setting message ( Header Length: " + messageSettings.getHeaderLength() + ", Header Byte: " + messageSettings.getHeaderSize() + " ).");
+        super("There are not enough header byte to receive the setting message (Header Length: " + messageSettings.getHeaderLength() + ", Header Byte: " + messageSettings.getHeaderSize() + ").");
     }
 }
