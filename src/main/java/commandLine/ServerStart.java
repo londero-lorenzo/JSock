69e82bb4253ec471a22ed7ea8539fead033222e6
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-06-15 23:52:41 +0200

    added Logger setting handler

--- src/main/java/commandLine/ServerStart.java
+++ src/main/java/commandLine/ServerStart.java
@@ -15,6 +15,13 @@ public class ServerStart {
 
     public static void main(String[] args) {
         Setting<Integer> headerSetting = new Setting<>(SettingTypes.HEADER_SETTING, 3);
+        new File("./Logs").mkdirs();
+        File logFile = new File("./Logs/SERVER_logFile.txt");
+        Logger logger = new Logger(Logger.Type.CONSOLE, logFile);
+
+        Setting<Logger> loggerSetting = new Setting<>(SettingTypes.LOGGER_SETTING, logger);
+
+        Settings settings = new Settings(loggerSetting);
         MessageSettings messageSettings = new MessageSettings(headerSetting);
         Address serverAddress = new Address("192.168.178.134", 30080);
         MultiConnectionServer multiConnectionServer = new MultiConnectionServer(serverAddress, messageSettings);
