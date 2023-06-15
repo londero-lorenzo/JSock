c286b40eacc852d2d3e1559ade5f28e1ceccf221
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-06-15 17:52:43 +0200

    replaced the padding in the string to convert to number

--- src/entities/java/socket/ByteList.java
+++ src/entities/java/socket/ByteList.java
@@ -29,7 +29,7 @@ public class ByteList extends structures.List<Byte> {
     }
 
     public int getIntFromByteList() {
-        return Integer.parseInt(this.getStringFromByteList());
+        return Integer.parseInt(this.getStringFromByteList().replace(" ", ""));
     }
 
 
