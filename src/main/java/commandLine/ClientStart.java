6d6ba30f781f2690a5d5bd99cba5159fe3afc242
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-06-15 22:40:28 +0200

    update the setting parameter passed, it is now choose from the SettingTypes enum

--- src/main/java/commandLine/ClientStart.java
+++ src/main/java/commandLine/ClientStart.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class ClientStart {
 
     public static void main(String[] args) {
-        Setting<Integer> headerSetting = new Setting<>(MessageSettings.HEADER_SETTING, 3);
+        Setting<Integer> headerSetting = new Setting<>(SettingTypes.HEADER_SETTING, 3);
         MessageSettings messageSettings = new MessageSettings(headerSetting);
         Client client = new Client(messageSettings);
         Address serverAddress = new Address("192.168.178.134", 30080);
