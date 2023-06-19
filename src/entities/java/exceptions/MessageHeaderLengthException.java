f3b23e4ac9c2b1b4fa1c2e1bdc89811b7011290c
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-06-19 23:25:35 +0200

    changed the superclass of this exception and linked the corresponding exception type

--- src/entities/java/exceptions/MessageHeaderLengthException.java
+++ src/entities/java/exceptions/MessageHeaderLengthException.java
@@ -2,8 +2,9 @@ package exceptions;
 
 import structures.MessageSettings;
 
-public class MessageHeaderLengthException extends RuntimeException {
+public class MessageHeaderLengthException extends SocketException {
     public MessageHeaderLengthException(MessageSettings messageSettings) {
         super("There are not enough header byte to receive the setting message (Header Length: " + messageSettings.getHeaderLength() + ", Header Byte: " + messageSettings.getHeaderSize() + ").");
+        this.setExceptionType(ExceptionTypes.MessageHeaderLengthException);
     }
 }
