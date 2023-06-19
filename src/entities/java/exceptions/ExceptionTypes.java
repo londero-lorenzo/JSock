633286f3cd40eca6cfb5e133d9e3b67304a608a2
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-06-19 23:38:39 +0200

    created an enum to storage all Exception types

---
+++ src/entities/java/exceptions/ExceptionTypes.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+
+public enum ExceptionTypes {
+    ExceptionNotInitialized,
+    InvalidIpv4AddressProvided,
+    InvalidPortProvided,
+    ConnectionRefused,
+    MessageHeaderLengthException,
+    ServerIsAlreadyRunningException,
+    SettingKeyNotFoundException,
+    UnalterableSettingByStringException,
+    TransferringSettingsException
+}
