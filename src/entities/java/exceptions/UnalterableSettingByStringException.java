239df796e6edafc61b8f1304ac7d5ae652438964
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-06-19 23:44:30 +0200

    changed the superclass of this exception and linked the corresponding exception type

--- src/entities/java/exceptions/UnalterableSettingByStringException.java
+++ src/entities/java/exceptions/UnalterableSettingByStringException.java
@@ -2,8 +2,9 @@ package exceptions;
 
 import structures.Setting;
 
-public class UnalterableSettingByStringException extends RuntimeException{
+public class UnalterableSettingByStringException extends SocketException {
     public UnalterableSettingByStringException(Setting<?> setting) {
         super("String type cannot be casted into '" + setting.getValue().getClass().getSimpleName() + "'");
+        this.setExceptionType(ExceptionTypes.UnalterableSettingByStringException);
     }
 }
